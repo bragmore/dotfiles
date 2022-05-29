6f9e1c69737829285097d0d16deefc5a0d20bf1e
bragmore <jonatan.siden@gmail.com>
2022-05-29 21:28:01 +0200

    new file structure for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,17 @@
 require("plugins") -- nvim plugins
 require("options") -- nvim options
 require("mappings") -- nvim keymappings
+require("user.colorizer")
+require("user.illuminate")
+require("user.nvimtree")
+require("user.treesitter")
+require("user.bufferline")
+require("user.colorscheme")
+require("user.kommentary")
+require("user.telescope")
+require("user.cmp")
+require("user.gitsigns")
+require("user.lualine")
+require("user.toggleterm")
+require("user.lsp.config")
+require("user.lsp.servers")
